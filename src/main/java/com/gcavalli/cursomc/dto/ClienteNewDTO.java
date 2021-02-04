1955be81fae6c0bb785fcafc1913223ccb199a64
Giancarlo Cavalli <gc.giancarloo@gmail.com>
2021-02-04 19:42:05 -0300

    Inserindo um novo Cliente com POST

---
+++ src/main/java/com/gcavalli/cursomc/dto/ClienteNewDTO.java
@@ -0,0 +1,153 @@
+package com.gcavalli.cursomc.dto;
+
+import java.io.Serializable;
+
+public class ClienteNewDTO implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private String nome;
+	private String email;
+	private String cpfOuCnpj;
+	private Integer tipo;
+	
+	private String telefone1;
+	private String telefone2;
+	private String telefone3;
+	
+	private String logradouro;
+	private String numero;
+	private String complemento;
+	private String bairro;
+	private String cep;
+	
+	private Integer cidadeId;
+	
+	public ClienteNewDTO() {
+	}
+
+	public ClienteNewDTO(String nome, String email, String cpfOuCnpj, Integer tipo, String telefone1, String telefone2,
+			String telefone3, String logradouro, String numero, String complemento, String bairro, String cep,
+			Integer cidadeId) {
+		super();
+		this.nome = nome;
+		this.email = email;
+		this.cpfOuCnpj = cpfOuCnpj;
+		this.tipo = tipo;
+		this.telefone1 = telefone1;
+		this.telefone2 = telefone2;
+		this.telefone3 = telefone3;
+		this.logradouro = logradouro;
+		this.numero = numero;
+		this.complemento = complemento;
+		this.bairro = bairro;
+		this.cep = cep;
+		this.cidadeId = cidadeId;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getCpfOuCnpj() {
+		return cpfOuCnpj;
+	}
+
+	public void setCpfOuCnpj(String cpfOuCnpj) {
+		this.cpfOuCnpj = cpfOuCnpj;
+	}
+
+	public Integer getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(Integer tipo) {
+		this.tipo = tipo;
+	}
+
+	public String getTelefone1() {
+		return telefone1;
+	}
+
+	public void setTelefone1(String telefone1) {
+		this.telefone1 = telefone1;
+	}
+
+	public String getTelefone2() {
+		return telefone2;
+	}
+
+	public void setTelefone2(String telefone2) {
+		this.telefone2 = telefone2;
+	}
+
+	public String getTelefone3() {
+		return telefone3;
+	}
+
+	public void setTelefone3(String telefone3) {
+		this.telefone3 = telefone3;
+	}
+
+	public String getLogradouro() {
+		return logradouro;
+	}
+
+	public void setLogradouro(String logradouro) {
+		this.logradouro = logradouro;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public String getComplemento() {
+		return complemento;
+	}
+
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+	public Integer getCidadeId() {
+		return cidadeId;
+	}
+
+	public void setCidadeId(Integer cidadeId) {
+		this.cidadeId = cidadeId;
+	}
+	
+	
+}
--- src/main/java/com/gcavalli/cursomc/services/ClienteService.java
+++ src/main/java/com/gcavalli/cursomc/services/ClienteService.java
@@ -9,27 +9,44 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.gcavalli.cursomc.domain.Cidade;
 import com.gcavalli.cursomc.domain.Cliente;
+import com.gcavalli.cursomc.domain.Endereco;
+import com.gcavalli.cursomc.domain.enums.TipoCliente;
 import com.gcavalli.cursomc.dto.ClienteDTO;
+import com.gcavalli.cursomc.dto.ClienteNewDTO;
+import com.gcavalli.cursomc.repositories.CidadeRepository;
 import com.gcavalli.cursomc.repositories.ClienteRepository;
+import com.gcavalli.cursomc.repositories.EnderecoRepository;
 import com.gcavalli.cursomc.services.exceptions.DataIntegrityException;
 import com.gcavalli.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class ClienteService {
-	
+
 	@Autowired
 	ClienteRepository repo;
+
+	@Autowired
+	EnderecoRepository enderecoRepository;
 	
+	@Autowired
+	CidadeRepository cidadeRepository;
+
 	public Cliente find(Integer id) {
 		Optional<Cliente> obj = repo.findById(id);
-		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: "+id+", Tipo: "+Cliente.class.getName()));
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
 	}
-	
+
+	@Transactional
 	public Cliente insert(Cliente obj) {
 		obj.setId(null);
-		return repo.save(obj);
+		obj = repo.save(obj);
+		enderecoRepository.saveAll(obj.getEnderecos());
+		return obj;
 	}
 	
 	public Cliente update(Cliente obj) {
@@ -37,23 +54,41 @@ public class ClienteService {
 		updateData(newObj, obj);
 		return repo.save(newObj);
 	}
-	
+
 	private void updateData(Cliente newObj, Cliente obj) {
 		newObj.setNome(obj.getNome());
 		newObj.setEmail(obj.getEmail());
 	}
-	
+
 	public void delete(Integer id) {
 		find(id);
 		try {
 			repo.deleteById(id);
-		} catch(DataIntegrityViolationException dive) {
+		} catch (DataIntegrityViolationException dive) {
 			throw new DataIntegrityException("Não é possível excluir um cliente com outros cadastros vinculados");
 		}
 	}
-	
+
 	public Cliente fromDto(ClienteDTO objDto) {
 		Cliente obj = new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+		return obj;
+	}
+
+	public Cliente fromDto(ClienteNewDTO objDto) {
+		Cliente obj = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfOuCnpj(),
+				TipoCliente.toEnum(objDto.getTipo()));
+		
+		obj.getTelefones().add(objDto.getTelefone1());
+		if (objDto.getTelefone2() != null)
+			obj.getTelefones().add(objDto.getTelefone2());
+		if (objDto.getTelefone3() != null)
+			obj.getTelefones().add(objDto.getTelefone3());
+		
+		Optional<Cidade> cid = cidadeRepository.findById(objDto.getCidadeId());
+		
+		Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(),
+				objDto.getBairro(), objDto.getCep(), obj, cid.get());
+		obj.getEnderecos().add(end);
 		return obj;
 	}
 
@@ -65,5 +100,5 @@ public class ClienteService {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
 	}
-	
+
 }
