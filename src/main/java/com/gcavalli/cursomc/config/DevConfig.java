7c48284975a79c4fb2908ad468023b8f89664e11
Giancarlo Cavalli <gc.giancarloo@gmail.com>
2021-02-11 21:06:28 -0300

    Criando profile de desenvolvimento

--- src/main/java/com/gcavalli/cursomc/config/DevConfig.java
+++ src/main/java/com/gcavalli/cursomc/config/DevConfig.java
@@ -3,6 +3,7 @@ package com.gcavalli.cursomc.config;
 import java.text.ParseException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -10,15 +11,21 @@ import org.springframework.context.annotation.Profile;
 import com.gcavalli.cursomc.services.DBService;
 
 @Configuration
-@Profile("test")
-public class TestConfig {
+@Profile("dev")
+public class DevConfig {
 	
 	@Autowired
-	DBService dbService;
+	private DBService dbService;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
 	
 	@Bean
 	public boolean instantiateDatabase() throws ParseException {
-		dbService.instantiateTestDatabase();
+		if(!"create".equals(strategy))
+			return false;
+		else
+			dbService.instantiateTestDatabase();
 		return true;
 	}
 	
