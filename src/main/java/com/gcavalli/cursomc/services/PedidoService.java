8dd7dfb948e722abec5dae0a12536d7ee0ab3515
Giancarlo Cavalli <gc.giancarloo@gmail.com>
2021-02-27 20:46:05 -0300

    Restricao de conteudo: cliente so recupera seus pedidos

--- src/main/java/com/gcavalli/cursomc/services/PedidoService.java
+++ src/main/java/com/gcavalli/cursomc/services/PedidoService.java
@@ -4,9 +4,13 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.gcavalli.cursomc.domain.Cliente;
 import com.gcavalli.cursomc.domain.ItemPedido;
 import com.gcavalli.cursomc.domain.PagamentoComBoleto;
 import com.gcavalli.cursomc.domain.Pedido;
@@ -14,6 +18,8 @@ import com.gcavalli.cursomc.domain.Produto;
 import com.gcavalli.cursomc.domain.enums.EstadoPagamento;
 import com.gcavalli.cursomc.repositories.ItemPedidoRepository;
 import com.gcavalli.cursomc.repositories.PedidoRepository;
+import com.gcavalli.cursomc.security.UserSS;
+import com.gcavalli.cursomc.services.exceptions.AuthorizationException;
 import com.gcavalli.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -44,6 +50,19 @@ public class PedidoService {
 		Optional<Pedido> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: "+id+", Tipo: "+Pedido.class.getName()));
 	}
+
+	//TODO findPage usando PageRequest.of e ClienteService ao inves de clienteRepository diretamente
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String direction, String orderBy) {
+		
+		UserSS user = UserService.authenticated(); 
+		if (user == null)
+			throw new AuthorizationException("Acesso negado");
+		
+		Cliente cli = clienteService.find(user.getId());
+		
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findByCliente(cli, pageRequest);
+	}
 	
 	@Transactional
 	public Pedido insert(Pedido obj) {
@@ -71,4 +90,6 @@ public class PedidoService {
 		emailService.sendOrderConfirmationHtmlEmail(obj);
 		return obj;
 	}
+	
+	
 }
